9db63ca4a9a316ae9bb836de793d69be4f1f5c8e
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-10-19 10:40:47 +0530

    Update hubconf file

--- hubconf.py
+++ hubconf.py
@@ -2,4 +2,4 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToT
