1e38315dcbb4435102c7da348ded2918bb5337d4
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-10-19 11:14:37 +0530

    commit once

--- hubconf.py
+++ hubconf.py
@@ -1,2 +1,10 @@
 import torch
 from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+transform_tensor_to_pil = ToPILImage()
+transform_pil_to_tensor = ToTensor()
+
+classes = ["T-shirt/top","Trouser","Pullover","Dress","Coat","Sandal","Shirt","Sneaker","Bag","Ankle boot",]
