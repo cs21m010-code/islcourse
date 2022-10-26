3a6b69bc6e162069430bb78206b723556043ef1c
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-10-26 11:20:03 +0530

    Update hubconf1.py

--- hubconf1.py
+++ hubconf1.py
@@ -4,9 +4,31 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, ToPILImage
 from PIL import Image
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
 
 transform_tensor_to_pil = ToPILImage()
 transform_pil_to_tensor = ToTensor()
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def load_data():
+
+    # Download training data from open datasets.
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    return training_data, test_data
