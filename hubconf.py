30efb4473d8faca683ca18c401752741f46a4466
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-10-26 11:53:27 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -32,3 +32,38 @@ def load_data():
         transform=ToTensor(),
     )
     return training_data, test_data
+
+
+def create_dataloaders(training_data, test_data, batch_size=64):
+
+    # Create data loaders.
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
+        
+    return train_dataloader, test_dataloader
+
+train_loader, test_loader = create_dataloaders(training_data, test_data, batch_size = 32)
+
+class cs21m010(nn.Module):
+    def __init__(self):
+        super(cs21m010, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 10)
+        )
+        self.sobj =torch.nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x =  self.linear_relu_stack(x)
+        x=self.sobj(x)
+
+        return x
+    
+y = (len(set([y for x,y in training_data])))
+model = cs21m010()
