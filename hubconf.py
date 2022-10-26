72b18e9eea39aac2964911e136ab45a015e09690
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-10-26 11:54:17 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -67,3 +67,32 @@ class cs21m010(nn.Module):
     
 y = (len(set([y for x,y in training_data])))
 model = cs21m010()
+
+#train the network
+def train_network(train_loader, optimizer,criteria, e):
+  for epoch in range(e):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        #print(outputs.shape, labels.shape)
+        tmp = torch.nn.functional.one_hot(labels, num_classes= 10)
+        loss = criteria(outputs, tmp)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+  print('Finished Training')
+
