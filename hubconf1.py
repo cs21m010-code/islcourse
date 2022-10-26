8ede188f2340dd5193ac20c6f681013b12e490c5
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-10-26 10:21:56 +0530

    Update hubconf1.py

--- hubconf1.py
+++ hubconf1.py
@@ -1,1 +1,9 @@
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, ToPILImage
+from PIL import Image
 
+transform_tensor_to_pil = ToPILImage()
+transform_pil_to_tensor = ToTensor()
