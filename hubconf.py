edfba9bd9b5d8822470ec165beed69b81b1e70d8
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-11-21 10:24:12 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,9 +1,16 @@
 import torch
 from torch import nn
 import torch.optim as optim
+import numpy as np
+import sklearn
 from sklearn import datasets
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs, make_circles, load_digits
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import homogeneity_score,completeness_score,v_measure_score
 
 ###### PART 1 ######
 
