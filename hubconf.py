d4623925896a44a5e92b65e003fd97f3da7187ba
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-11-21 09:50:10 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
 import torch.optim as optim
-
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs, make_circles
 ###### PART 1 ######
