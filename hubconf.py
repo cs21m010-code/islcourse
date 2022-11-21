a55a774154b1a393225ca5f8063db020d8b94c3a
cs21m010iittp <99287117+cs21m010iittp@users.noreply.github.com>
2022-11-21 10:03:49 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -6,3 +6,7 @@ from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs, make_circles, load_digits
 
 ###### PART 1 ######
+
+def get_data_blobs(n_points=100):
+  X, y = make_blobs(n_samples=n_points)
+  return X,y
